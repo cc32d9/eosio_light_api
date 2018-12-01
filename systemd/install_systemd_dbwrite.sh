a16b58c7e154db7a5331580238dc1f7450c87bc7
cc32d9 <cc32d9@gmail.com>
2018-12-01 02:50:03 +0000

    bugfixes in systemd files

--- systemd/install_systemd_dbwrite.sh
+++ systemd/install_systemd_dbwrite.sh
@@ -1,4 +1,9 @@
-NETWORKS="eos"
+NETWORKS="$@"
+
+if [ x"$NETWORKS" = x ]; then 
+	echo "Need network names as arguments" 1>&2
+	exit 1;
+fi
 
 for n in $NETWORKS; do
     if [ ! -r /etc/default/lightapi_${n} ]; then
@@ -14,6 +19,7 @@ systemctl daemon-reload
 for n in $NETWORKS; do
     systemctl enable lightapi_dbwrite@${n}.service
     systemctl start lightapi_dbwrite@${n}.service
+    echo installed lightapi_dbwrite@${n}.service
 done
 
 
