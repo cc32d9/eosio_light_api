5c1268f4fa77f2194661266eb9bcd782ecb67e4b
Ben Sigman <bensig@gmail.com>
2023-02-25 11:28:53 -0800

    update for Libre and make README more accessible

---
+++ setup/add_libre_mainnet.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+perl /opt/eosio_light_api/scripts/lightapi_addnet.pl \
+     --network=libre \
+     --chainid=38b1d7815474d0c60683ecbea321d723e83f5da6ae5f1c1f9fecc69d9ba96465 \
+     --descr="Libre" --token=LIBRE --dec=4 ${LIGHTAPI_DB_OPTS}
